05b494c4b5984e123b3e906fa837544c2c5b99af
baf-03 <bilalahmedfarooqi03@gmail.com>
2024-01-29 23:27:02 +0500

    adding Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-MVB30ZHEVV"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MVB30ZHEVV', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
+
       <body className={inter.className}>
-      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      
-        {children}
-      </AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
