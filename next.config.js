74a71043cfe12d6939fc61183acb17f098992ab6
baf-03 <bilalahmedfarooqi03@gmail.com>
2024-02-23 20:43:01 +0500

    adding loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-      domains: ['eiharold.com',"grademiners.com","rawts.com.pk","assets.zyrosite.com"], // Add the domain you need
+      domains: ['eiharold.com',"grademiners.com","rawts.com.pk","assets.zyrosite.com","cdn.dribbble.com"], // Add the domain you need
     },
   };
