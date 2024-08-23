2c6b0d4197f1450ceade14b7ef694e3340d59b91
baf-03 <bilalahmedfarooqi03@gmail.com>
2024-08-24 01:11:19 +0500

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://bilalahmedbaf-03.netlify.app', 
-    generateRobotsTxt: true, 
-    // ...other options
+    siteUrl: 'https://bilalahmedbaf-03.netlify.app', // Replace with your actual site URL
+    generateRobotsTxt: true, // Generate a robots.txt file
+    sitemapSize: 5000, // Max entries per sitemap file (optional)
+    changefreq: 'daily', // Change frequency of your pages (optional)
+    priority: 0.7, // Priority of URLs (optional)
   }
-  
