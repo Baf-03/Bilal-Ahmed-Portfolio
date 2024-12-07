0c342a60b8ade2f4c4bb67af6e573402182f4e8b
Bilal Ahmed <123253713+Baf-03@users.noreply.github.com>
2024-12-07 05:34:34 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/router"; // Router hook
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import Introduction from "@/app/Components/LandingPage/LandingPage";
 import CreativeProcess from "./Components/CreativeProcess/CreativeProcess";
@@ -14,37 +12,34 @@ import ContactForm from "./Components/ContactForm";
 import Projects from "./Components/Projects/Projects";
 import Footer from "./Components/Footer/Footer";
 import WhatsAppButton from "./Components/WhatsAppbtn";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const [darkmode, setDarkMode] = useState(false);
   const [loading, setLoading] = useState(true);
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("element");
 
-  // Retrieve dark mode setting
   useEffect(() => {
     const storedDarkMode = localStorage.getItem("darkmode");
     if (storedDarkMode !== null) {
       setDarkMode(JSON.parse(storedDarkMode));
     }
-    setLoading(false); // Finish loading setup
+    setLoading(false); // Turn off loading after initial setup
   }, []);
 
-  // Locomotive Scroll setup
   useEffect(() => {
     let scroll: any = null;
 
     const initializeLocomotiveScroll = async () => {
-      if (
-        typeof window !== "undefined" &&
-        window.innerWidth >= 768 &&
-        scrollContainerRef.current
-      ) {
+      if (typeof window !== "undefined" && window.innerWidth >= 768 && scrollContainerRef.current) {
         const LocomotiveScrollModule = await import("locomotive-scroll");
         scroll = new LocomotiveScrollModule.default({
           el: scrollContainerRef.current,
           smooth: true,
           multiplier: 0.8,
+          getDirection: true,
           reloadOnContextChange: true,
         });
 
@@ -71,19 +66,14 @@ export default function Home() {
     };
   }, [loading]);
 
-  // Navigate to specific sections if the query parameter is present
   useEffect(() => {
-    if (!router.isReady) return;
-
-    const searchParams = new URLSearchParams(window.location.search);
-    const targetElementId = searchParams.get("element");
-    if (targetElementId) {
-      const targetElement = document.getElementById(targetElementId);
+    if (search) {
+      const targetElement = document.getElementById(search);
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: "smooth" });
       }
     }
-  }, [router]);
+  }, [search]);
 
   return (
     <>
@@ -92,33 +82,34 @@ export default function Home() {
           <div className="loader"></div>
         </div>
       ) : (
-        <main
-          className="transition-colors duration-500"
-          ref={scrollContainerRef}
-          data-scroll-container
-        >
-          <div className="w-[98%] lg:w-[94%] xl:w-[90vw] m-auto flex flex-col gap-12 justify-center items-center mt-2">
-            <Introduction />
-            <CreativeProcess />
-            <NeedofSp />
-            <TechUsed />
-            <Skills />
-            <Projects />
-            <ActivitiesTimeline />
-            <Education />
-            <ContactForm />
-          </div>
-          <Footer />
-          <div className="bg-[#3b82f6] w-[100%] text-center">
-            Portfolio is currently in development phase
-            <br />
-            Projects, Education Sections, and Blogs page
-            <br />
-            and many more features are coming soon
-          </div>
+        <>
+          <main
+            className="transition-colors duration-500"
+            ref={scrollContainerRef}
+            data-scroll-container
+          >
+            <div className="w-[98%] lg:w-[94%] xl:w-[90vw] m-auto flex flex-col gap-12 justify-center items-center mt-2">
+              <Introduction />
+              <CreativeProcess />
+              <NeedofSp />
+              <TechUsed />
+              <Skills />
+              <Projects />
+              <ActivitiesTimeline />
+              <Education />
+              <ContactForm />
+            </div>
+            <Footer />
+            <div className="bg-[#3b82f6] w-[100%] text-center">
+              Portfolio is currently in development phase
+              <br />
+              Projects, Education Sections and Blogs page <br />
+              and many more features are coming soon
+            </div>
+          </main>
           <WhatsAppButton />
-        </main>
+        </>
       )}
-    </>
-  );
+    </>
+  );
 }
